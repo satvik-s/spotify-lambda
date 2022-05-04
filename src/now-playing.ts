aff3b266cc2eea46d10d1968657dfc9edb3dcb6b
Satvik Shukla <satvik.shukla@outlook.com>
2022-05-03 21:05:56 -0700

    add playlist info

--- src/now-playing.ts
+++ src/now-playing.ts
@@ -8,7 +8,7 @@ const { CLIENT_ID, CLIENT_SECRET, REFRESH_TOKEN } = process.env;
 const NOW_PLAYING_ENDPOINT =
     'https://api.spotify.com/v1/me/player/currently-playing';
 
-async function getNowPlaying() {
+async function getNowPlaying(queryParams?: Record<string, string>) {
     const { access_token: accessToken } = await getAccessToken();
 
     try {
@@ -21,9 +21,25 @@ async function getNowPlaying() {
 
         const data = response.data;
         if (data.is_playing && data.currently_playing_type === 'track') {
+            const trackInfo = getTrackInfo(data.item);
+
+            if (
+                queryParams?.detailed === 'true' &&
+                data.context?.type === 'playlist'
+            ) {
+                const playlistInfo = await getPlaylistInfo(
+                    data.context?.href,
+                    accessToken,
+                );
+                return {
+                    is_playing: true,
+                    ...trackInfo,
+                    ...playlistInfo,
+                };
+            }
             return {
                 is_playing: true,
-                ...getTrackInfo(data.item),
+                ...trackInfo,
             };
         }
         return {
@@ -38,12 +54,28 @@ async function getNowPlaying() {
     }
 }
 
-export async function main(_event: unknown) {
+async function getPlaylistInfo(href: string, accessToken: string) {
+    const response = await axios.get(href, {
+        headers: {
+            Authorization: `Bearer ${accessToken}`,
+        },
+    });
+    const data = response.data;
+
+    return {
+        playlist: data.name,
+        playlist_url: data.external_urls?.spotify,
+    };
+}
+
+export async function main(event: {
+    queryStringParameters?: Record<string, string>;
+}) {
     if (CLIENT_ID === '' || CLIENT_SECRET === '' || REFRESH_TOKEN === '') {
         throw new Error('secrets not initialized');
     }
 
-    const nowPlaying = await getNowPlaying();
+    const nowPlaying = await getNowPlaying(event.queryStringParameters);
     console.log(JSON.stringify(nowPlaying, null, 2));
 
     return {
