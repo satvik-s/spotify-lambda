0f63704c6813ff721a5f8ab3e119003db74e7443
Satvik Shukla <satvik.shukla@outlook.com>
2022-05-03 17:58:57 -0700

    add album url

--- src/utils/track.ts
+++ src/utils/track.ts
@@ -1,11 +1,12 @@
 export function getTrackInfo(track: any) {
     return {
+        album: track?.album?.name,
+        album_image_url: track?.album?.images?.[0]?.url,
+        album_url: track?.album?.external_urls?.spotify,
         artists: track?.artists
             ?.map((artist: { name: string }) => artist.name)
             .join(', '),
         track_name: track?.name,
-        album: track?.album?.name,
-        album_image_url: track?.album?.images?.[0]?.url,
         track_url: track?.external_urls?.spotify,
     };
 }
